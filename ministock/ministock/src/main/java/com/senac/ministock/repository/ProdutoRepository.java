a106dcd9e59eb2d04c541c4b47e36e9b44da54d0
Pablo <pablomaia533@gmail.com>
2025-08-26 12:02:12 -0300

    Delete ministock/ministock directory

--- ministock/ministock/src/main/java/com/senac/ministock/repository/ProdutoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.senac.ministock.repository;
-
-import com.senac.ministock.entity.Produto;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
-}
--- ministock/ministock/src/main/java/com/senac/ministock/repository/UsuarioRepository.java
+++
@@ -1,7 +0,0 @@
-package com.senac.ministock.repository;
-
-import com.senac.ministock.entity.Usuario;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
-}
