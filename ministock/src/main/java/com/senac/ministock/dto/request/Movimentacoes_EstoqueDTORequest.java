cf93a519b2525159919f4fe5c688c1bb0adba67a
Pablo <pablomaia533@gmail.com>
2025-09-02 11:46:17 -0300

    Add files via upload

---
+++ ministock/src/main/java/com/senac/ministock/dto/request/Movimentacoes_EstoqueDTORequest.java
@@ -0,0 +1,80 @@
+package com.senac.ministock.dto.request;
+
+import java.math.BigDecimal;
+
+
+public class Movimentacoes_EstoqueDTORequest {
+
+    private String tipo; // ENTRADA / SAIDA
+
+
+    private Integer quantidade;
+
+    private String observacao;
+
+    private BigDecimal precoCompra;
+
+    private BigDecimal precoVenda;
+
+
+    private Integer produtoId;
+
+
+    private String usuarioId;
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public Integer getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(Integer quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public String getObservacao() {
+        return observacao;
+    }
+
+    public void setObservacao(String observacao) {
+        this.observacao = observacao;
+    }
+
+    public BigDecimal getPrecoCompra() {
+        return precoCompra;
+    }
+
+    public void setPrecoCompra(BigDecimal precoCompra) {
+        this.precoCompra = precoCompra;
+    }
+
+    public BigDecimal getPrecoVenda() {
+        return precoVenda;
+    }
+
+    public void setPrecoVenda(BigDecimal precoVenda) {
+        this.precoVenda = precoVenda;
+    }
+
+    public Integer getProdutoId() {
+        return produtoId;
+    }
+
+    public void setProdutoId(Integer produtoId) {
+        this.produtoId = produtoId;
+    }
+
+    public String getUsuarioId() {
+        return usuarioId;
+    }
+
+    public void setUsuarioId(String usuarioId) {
+        this.usuarioId = usuarioId;
+    }
+}
---
+++ ministock/src/main/java/com/senac/ministock/dto/response/UsuarioDTOResponse.java
@@ -0,0 +1,61 @@
+package com.senac.ministock.dto.response;
+
+import java.time.LocalDateTime;
+
+
+public class UsuarioDTOResponse {
+    private String id;
+    private String nome;
+    private String email;
+    private String perfil;
+    private LocalDateTime dataCriacao;
+    private LocalDateTime ultimoLogin;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPerfil() {
+        return perfil;
+    }
+
+    public void setPerfil(String perfil) {
+        this.perfil = perfil;
+    }
+
+    public LocalDateTime getDataCriacao() {
+        return dataCriacao;
+    }
+
+    public void setDataCriacao(LocalDateTime dataCriacao) {
+        this.dataCriacao = dataCriacao;
+    }
+
+    public LocalDateTime getUltimoLogin() {
+        return ultimoLogin;
+    }
+
+    public void setUltimoLogin(LocalDateTime ultimoLogin) {
+        this.ultimoLogin = ultimoLogin;
+    }
+}
