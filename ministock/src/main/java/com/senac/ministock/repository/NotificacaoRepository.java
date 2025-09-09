ec890a0f83e1d97462b4b2fa5b8183a70a5e70d3
Pablo <pablomaia533@gmail.com>
2025-09-09 11:31:38 -0300

    Delete ministock directory

--- ministock/src/main/java/com/senac/ministock/repository/NotificacaoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.senac.ministock.repository;
-
-import com.senac.ministock.entity.Notificacao;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface NotificacaoRepository extends JpaRepository<Notificacao,Integer> {
-}
