6170a4f112ea6fe8f0ea7215149076deaf80908c
Pablo <pablomaia533@gmail.com>
2025-09-15 08:22:02 -0300

    corrigido

---
+++ src/main/java/com/senac/ministock/config/ModelMapperConfig.java
@@ -0,0 +1,2 @@
+package com.senac.ministock.config;public class ModelMapperConfig {
+}
--- src/main/java/com/senac/ministock/controller/Movimentacoes_EstoqueController.java
+++ src/main/java/com/senac/ministock/controller/Movimentacoes_EstoqueController.java
@@ -1,25 +1,66 @@
 package com.senac.ministock.controller;
 
+
+import com.senac.ministock.dto.request.Movimentacoes_EstoqueDTORequest;
+import com.senac.ministock.dto.request.Movimentacoes_EstoqueDTOUpdateRequest;
+import com.senac.ministock.dto.response.Movimentacoes_EstoqueDTOResponse;
+import com.senac.ministock.dto.response.Movimentacoes_EstoqueDTOUpdateResponse;
 import com.senac.ministock.entity.Movimentacoes_Estoque;
 import com.senac.ministock.service.Movimentacoes_EstoqueService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("api/movimentacoes-estoque")
-public class Movimentacoes_EstoqueController {
-    private Movimentacoes_EstoqueService movimentacoesEstoqueService;
+@RequestMapping("api/movimentacao_estoque")
+public class Movimentacao_EstoqueController {
 
-    public Movimentacoes_EstoqueController(Movimentacoes_EstoqueService movimentacoesEstoqueService) {
-        this.movimentacoesEstoqueService = movimentacoesEstoqueService;
+    private Movimentacoes_EstoqueService service;
+
+    public void MovimentacaoEstoqueController(Movimentacoes_EstoqueService service) {
+        this.service = service;
+    }
+
+    public Movimentacao_EstoqueController(Movimentacoes_EstoqueService service) {
+        this.service = service;
     }
 
     @GetMapping("/listar")
-    public ResponseEntity<List<Movimentacoes_Estoque>> listarMovimentacoesEstoque() {
-        return ResponseEntity.ok(movimentacoesEstoqueService.listarMovimentacoesEstoque());
+    public ResponseEntity<List<Movimentacoes_EstoqueDTOResponse>> listar() {
+        return ResponseEntity.ok(service.listarMovimentacoes());
+    }
+
+    @GetMapping("/listar_por_id/{movimentacao_id}")
+    public ResponseEntity<Movimentacoes_EstoqueDTOResponse> listarPorId(@PathVariable("movimentacao_id") Integer id) {
+        Movimentacoes_EstoqueDTOResponse m = service.listarPorId(id);
+        return (m != null) ? (ResponseEntity<Movimentacoes_EstoqueDTOResponse>) ResponseEntity.ok() : ResponseEntity.noContent().build();
+    }
+
+    @PostMapping("/criar")
+    public ResponseEntity<Movimentacoes_EstoqueDTOResponse> criar(@Valid @RequestBody Movimentacoes_EstoqueDTORequest dto) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(service.criarMovimentacao(dto));
+    }
+
+    @PutMapping("/atualizar/{movimentacao_id}")
+    public ResponseEntity<Movimentacoes_EstoqueDTOResponse> atualizar(
+            @PathVariable("movimentacao_id") Integer id,
+            @Valid @RequestBody Movimentacoes_EstoqueDTOUpdateRequest dto) {
+        return ResponseEntity.ok(service.atualizarMovimentacao(id, dto));
+    }
+
+    @PatchMapping("/atualizar_status/{movimentacao_id}")
+    public ResponseEntity<Movimentacoes_EstoqueDTOUpdateResponse> atualizarStatus(
+            @PathVariable("movimentacao_id") Integer id,
+            @Valid @RequestBody Movimentacoes_EstoqueDTORequest dto) {
+        return ResponseEntity.ok(service.atualizarStatusMovimentacao(id, dto));
+    }
+
+    @DeleteMapping("/apagar/{movimentacao_id}")
+    public ResponseEntity<Void> apagar(@PathVariable("movimentacao_id") Integer id) {
+        service.apagarMovimentacao(id);
+        return ResponseEntity.noContent().build();
     }
 }
