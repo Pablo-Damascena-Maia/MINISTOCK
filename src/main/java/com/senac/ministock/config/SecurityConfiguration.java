8743b7a5e5e18acb384b58d806a504862d7a865f
Pablo <pablomaia533@gmail.com>
2025-10-07 11:56:25 -0300

    security add

---
+++ src/main/java/com/senac/ministock/config/SecurityConfiguration.java
@@ -0,0 +1,109 @@
+package com.senac.ministock.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+    @Autowired
+    private UsuarioAuthenticationFilter usuarioAuthenticationFilter;
+
+    public static final String [] ENDPOINTS_WITH_AUTHENTICATION_NOT_REQUIRED = {
+            "/api/usuario/email",
+            "/api/usuario/criar",
+
+
+
+            "/v3/api-docs/**",
+            "/swagger-ui/**",
+            "/swagger-ui.html"
+    };
+
+    // Endpoints que requerem autenticação para serem acessados
+    public static final String [] ENDPOINTS_WITH_AUTHENTICATION_REQUIRED = {
+            "/api/usuario/atualizar/{id}",
+            "/api/usuario/atualizarStatus/{id}",
+            "/api/usuario/listar",
+            "/api/usuario/listarPorId/{id}",
+            "/api/usuario/apagar/{id}",
+
+            "/api/produto/atualizar/{id}",
+            "/api/produto/criar",
+            "/api/produto/atualizarStatus/{id}",
+            "/api/produto/listar",
+            "/api/produto/listarPorId/{id}",
+            "/api/produto/apagar/{id}",
+
+            "/api/notificacao/atualizar/{id}",
+            "/api/notificacao/criar",
+            "/api/notificacao/marcarComoLida/{id}",
+            "/api/notificacao/atualizarStatus/{id}",
+            "/api/notificacao/listar",
+            "/api/notificacao/listarPorNotificacaoId/{id}",
+            "/api/notificacao/apagar/{id}",
+
+            "/api/movimentacoes_estoque/atualizar/{id}",
+            "/api/movimentacoes_estoque/criar",
+            "/api/movimentacoes_estoque/listar",
+            "/api/movimentacoes_estoque/apagar/{id}",
+
+            "/api/configuracao/atualizar/{configuracaoId}",
+            "/api/configuracao/criar",
+            "/api/configuracao/atualizarStatus/{configuracaoId}",
+            "/api/configuracao/listar",
+            "/api/configuracao/listarPorId/{configuracaoId}",
+            "/api/configuracao/apagar/{configuracaoId}",
+
+
+    };
+
+    // Endpoints que só podem ser acessador por usuários com permissão de cliente
+    public static final String [] ENDPOINTS_CUSTOMER = {
+            "/jogo"
+    };
+
+    // Endpoints que só podem ser acessador por usuários com permissão de administrador
+    public static final String [] ENDPOINTS_ADMIN = {
+            "/categoria"
+    };
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(ENDPOINTS_WITH_AUTHENTICATION_NOT_REQUIRED).permitAll()
+                        .requestMatchers(ENDPOINTS_ADMIN).hasRole("ADMINISTRATOR")
+                        .requestMatchers(ENDPOINTS_CUSTOMER).hasRole("CUSTOMER")
+                        .requestMatchers(ENDPOINTS_WITH_AUTHENTICATION_REQUIRED).authenticated()
+                        .anyRequest().denyAll()
+                )
+                .addFilterBefore(usuarioAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+}
